1962edaa4c7c4d0a590ce99e0c6f41d2d3624352
viksafe <andreas@vikerup.se>
2021-10-28 15:13:46 +0200

    Update invoke-cacklarn.ps1

--- invoke-cacklarn.ps1
+++ invoke-cacklarn.ps1
@@ -3,20 +3,27 @@ function invoke-cacklarn() {
         [Parameter(Mandatory=$true)][string]$path,
         [switch]$recurse
     )
-[array]$folder = (get-childitem $path).directory.fullname
-[array]$folder += (get-childitem $path -Recurse:$recurse).FullName
+[array]$folder = (get-childitem $path).directory | select -expand PsPath
+[array]$folder += (get-childitem $path -Recurse:$recurse) | select -expand PsPath
 $folder = $folder | sort -Unique
 
 foreach($item in $folder){
-$permission = (Get-Acl $item -ErrorAction SilentlyContinue).Access | ?{$_.IdentityReference -in "BUILTIN\Users","Everyone"} | Select IdentityReference,FileSystemRights
-$permission | Add-Member -MemberType NoteProperty -name "path" -Value $item -passthru -ErrorAction SilentlyContinue | out-null
+$permission = (Get-Acl $item -ErrorAction SilentlyContinue).Access | ?{$_.IdentityReference -in "$env:USERDOMAIN\$env:USERNAME","BUILTIN\Users","Everyone"} | Select IdentityReference,FileSystemRights,RegistryRights
+$permission | Add-Member -MemberType NoteProperty -name path -Value $($item | Split-Path -NoQualifier) -passthru -ErrorAction SilentlyContinue | out-null
 $permission | where {
                    $_.FileSystemRights -like "*CreateFiles*" -or 
                    $_.FileSystemRights -like "*AppendData*" -or 
                    $_.FileSystemRights -like "*FullControl*" -or
                    $_.FileSystemRights -like "*Modify*" -or
                    $_.FileSystemRights -like "*Write*" -or
-                   $_.FileSystemRights -like "*Delete*"
+                   $_.FileSystemRights -like "*Delete*" -or 
+		   ## registry
+                   $_.RegistryRights -like "*CreateFiles*" -or 
+                   $_.RegistryRights -like "*AppendData*" -or 
+                   $_.RegistryRights -like "*FullControl*" -or
+                   $_.RegistryRights -like "*Modify*" -or
+                   $_.RegistryRights -like "*Write*" -or
+                   $_.RegistryRights -like "*Delete*"
                    }
 }
 }
