793fe634efea078ad9dc8231ee1e3bd50cc72c8e
viksafe <andreas@vikerup.se>
2021-10-28 11:03:21 +0200

    Update invoke-cacklarn.ps1

--- invoke-cacklarn.ps1
+++ invoke-cacklarn.ps1
@@ -3,12 +3,12 @@ function invoke-cacklarn() {
         [Parameter(Mandatory=$true)][string]$path,
         [switch]$recurse
     )
-[array]$folder = (get-childitem $path).directory.fullname
-[array]$folder += (get-childitem $path -Recurse:$recurse).FullName
+[array]$folder = (get-childitem $path -ErrorAction SilentlyContinue).directory.fullname
+[array]$folder += (get-childitem $path -ErrorAction SilentlyContinue -Recurse:$recurse).FullName
 $folder = $folder | sort -Unique
 
 foreach($item in $folder){
-$permission = (Get-Acl $item -ErrorAction SilentlyContinue).Access | ?{$_.IdentityReference -eq "BUILTIN\Users"} | Select IdentityReference,FileSystemRights
+$permission = (Get-Acl $item -ErrorAction SilentlyContinue).Access | ?{$_.IdentityReference -in "BUILTIN\Users","Everyone"} | Select IdentityReference,FileSystemRights
 $permission | Add-Member -MemberType NoteProperty -name "path" -Value $item -passthru -ErrorAction SilentlyContinue | out-null
 $permission | where {
                    $_.FileSystemRights -like "*CreateFiles*" -or 
